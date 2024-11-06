d7848523c1abe3df1288b7f21abc1535d2feeafd
Mike <mikemonchu@gmail.com>
2024-11-06 22:10:12 +0200

    updated port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull mike164/simply-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 mike164/simply-python-flask-app
+docker run -d -p 5001:5001 mike164/simply-python-flask-app
